19240b0d9597891414417803949c205324113297
timakultin-max <tima.kultin@gmail.com>
2026-09-20 09:46:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ let moveTimeout = null;
 
 // Отслеживаем загруженные области, чтобы не запрашивать дважды
 const loadedBoxes = [];
-const MAX_BOX_SIZE = 0.04; // ~4 км
+const MAX_BOX_SIZE = 0.08; // ~8 км
 
 function isBoxLoaded(south, west, north, east) {
   const cx = (west + east) / 2;
